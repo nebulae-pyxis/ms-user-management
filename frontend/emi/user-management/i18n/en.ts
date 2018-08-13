17729f675397168fe9b1cbca4201a5eeb0812b7a
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-08-13 16:46:17 -0500

    Changes filename to gateway

--- frontend/emi/user-management/i18n/en.ts
+++ frontend/emi/user-management/i18n/en.ts
@@ -1,6 +1,13 @@
 export const locale = {
   lang: 'en',
   data: {
-    
+    USER: 'Users',
+      SEARCH: 'Search',
+      USERNAME: 'Username',
+      DOC_TYPE: 'Document type',
+      CC: 'Indetification card',
+      TI: 'Identity card',
+      DOC_ID: 'Document ID',
+      ACTIVE: 'Active'
   }
 };
--- frontend/emi/user-management/i18n/es.ts
+++ frontend/emi/user-management/i18n/es.ts
@@ -1,6 +1,15 @@
 export const locale = {
   lang: 'es',
   data: {
-
+    USER: {
+      USER: 'Usuarios',
+      SEARCH: 'Buscar',
+      USERNAME: 'Nombre de usuario',
+      DOC_TYPE: 'Tipo documento',
+      CC: 'Cédula',
+      TI: 'Tarjeta de identidad',
+      DOC_ID: 'ID documento',
+      ACTIVE: 'Activo'
+    }
   }
 };
