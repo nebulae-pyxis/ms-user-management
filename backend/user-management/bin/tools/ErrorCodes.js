89199f28af491b4ec6e40720a7f121a892716279
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-10-30 15:02:14 -0500

    Improves error message

--- backend/user-management/bin/tools/ErrorCodes.js
+++ backend/user-management/bin/tools/ErrorCodes.js
@@ -8,7 +8,7 @@ const INVALID_USERNAME_FORMAT_ERROR_CODE = {code: 16012, description: 'Invalid u
 const EMAIL_ALREADY_USED_ERROR_CODE = {code: 16014, description: 'Email already used'};
 const USER_UPDATE_OWN_INFO_ERROR_CODE = {code: 16015, description: 'You cannot update your own info'};
 const USER_BELONG_TO_OTHER_BUSINESS_ERROR_CODE = {code: 16016, description: 'User belongs to other business'};
-const INVALID_USER_CREDENTIALS_ERROR_CODE = {code: 16017, description: 'Invalid user credentials'};
+const INVALID_USER_CREDENTIALS_OR_TOKEN_ERROR_CODE = {code: 16017, description: 'Invalid user credentials or token'};
 
 
 module.exports =  { 
@@ -20,5 +20,5 @@ module.exports =  {
     INVALID_USERNAME_FORMAT_ERROR_CODE,
     USER_UPDATE_OWN_INFO_ERROR_CODE,
     USER_BELONG_TO_OTHER_BUSINESS_ERROR_CODE,
-    INVALID_USER_CREDENTIALS_ERROR_CODE
+    INVALID_USER_CREDENTIALS_OR_TOKEN_ERROR_CODE
 } 
