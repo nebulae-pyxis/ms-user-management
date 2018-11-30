9b92fbc8fcff9b0b25f9baba2421ef347da19bc1
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-11-30 11:06:33 -0500

    Fixes errors in backend

--- backend/user-management/bin/domain/UserValidatorHelper.js
+++ backend/user-management/bin/domain/UserValidatorHelper.js
@@ -236,7 +236,7 @@ class UserValidatorHelper {
               temporary: userPassword.temporary || false,
               value: userPassword.password
             },
-            businessId: !data.args ? undefined : (data.args.business ? data.args.business.trim(): undefined)
+            businessId: !data.args ? undefined : (data.args.businessId ? data.args.businessId.trim(): undefined)
           };
 
           if (!user.id || !userPassword || !userPassword.password) {
@@ -273,7 +273,7 @@ class UserValidatorHelper {
           const user = {
             id: !data.args ? undefined : data.args.userId,
             userRoles: !data.args ? undefined : data.args.input,
-            businessId: !data.args ? undefined : (data.args.business ? data.args.business.trim(): undefined)
+            businessId: !data.args ? undefined : (data.args.businessId ? data.args.businessId.trim(): undefined)
           };
           if (!user.id || !user.userRoles) {
             return this.createCustomError$(
